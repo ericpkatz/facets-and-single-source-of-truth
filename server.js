bde5031e2f79daf43aefc9cc0e7baec297bee5c7
Eric P. Katz <ericpkatz@gmail.com>
2018-06-10 16:30:57 -0400

    add backend

--- server.js
+++ server.js
@@ -7,6 +7,45 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+app.get('/api/users', (req, res, next)=> {
+  let where = {};
+  if(req.query.where){
+    where = JSON.parse(req.query.where);
+  }
+  User.findAll({ where })
+    .then( users => res.send(users))
+    .catch(next);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/my_db');
+
+const User = conn.define('user', {
+  name: Sequelize.STRING,
+  active: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true
+  },
+  state: {
+    type: Sequelize.STRING,
+    defaultValue: ''
+  },
+  role: {
+    type: Sequelize.STRING,
+    defaultValue: ''
+  }
+});
+
+const data = [
+  { name: 'moe', active: true, state: 'NY', role: 'staff' },
+  { name: 'larry', active: true, state: 'NJ', role: 'staff' },
+  { name: 'curly', active: false, state: 'CT', role: 'other' },
+  { name: 'shep', active: false, state: 'NJ', role: 'staff' },
+];
+conn.sync({ force: true })
+  .then( ()=> {
+    return Promise.all(data.map( user => User.create(user)));
+  });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import _ from 'underscore';
+import axios from 'axios';
 
 const root = document.getElementById('root');
 
@@ -24,10 +25,25 @@ class Users extends Component{
     super();
     const filter = generateFilter(location.search);
     this.state = {
-      filter
+      filter,
+      users: []
     };
-    console.log('load data for ', filter);
     this.onChange = this.onChange.bind(this);
+    this.load = this.load.bind(this);
+    this.load(filter);
+  }
+  load(filter){
+    const cleanFilter = Object.keys(filter).reduce((memo, key)=> {
+      if(filter[key]){
+        const val = filter[key];
+        const isBool = val === 'true' || val === 'false';
+        memo[key] = isBool ? Boolean(val) : val;
+      }
+      return memo;
+    }, {});
+    axios.get(`/api/users?where=${JSON.stringify(cleanFilter)}`)
+      .then( result => result.data)
+      .then( users => this.setState({ users }));
   }
   onChange(ev){
     const change = {};
@@ -39,14 +55,14 @@ class Users extends Component{
     const filter = generateFilter(nextProps.location.search);
     if(!_.isEqual(filter, this.state.filter)){
       this.setState({ filter });
-      console.log('load data for ', filter);
+      this.load(filter);
     }
   }
   render(){
     const { onChange } = this;
-    const { filter } = this.state;
+    const { filter, users } = this.state;
     return (
-      <_Users filter={ filter } onChange={ onChange }/>
+      <_Users filter={ filter } onChange={ onChange } users={ users }/>
     );
   }
 }
@@ -85,30 +101,50 @@ const roleOptions = [
   { text: 'Other', value: 'other' },
 ];
 
-const _Users = ({ filter, onChange })=> {
+const _Users = ({ filter, onChange, users })=> {
   return (
     <div>
       <h1>Users</h1>
-      <Facet options={ activeOptions } name='active' filter={ filter } onChange={ onChange }/>
-      <Facet options={ stateOptions } name='state' filter={ filter }  onChange={ onChange } />
-      <Facet options={ roleOptions } name='role' filter={ filter }   onChange={ onChange }/>
+      <div style={{ display: 'flex', justifyContent: 'space-around', borderBottom: 'solid 1px black'}}>
+        <Facet options={ activeOptions } name='active' filter={ filter } onChange={ onChange }/>
+        <Facet options={ stateOptions } name='state' filter={ filter }  onChange={ onChange } />
+        <Facet options={ roleOptions } name='role' filter={ filter }   onChange={ onChange }/>
+      </div>
+      <ul>
+      {
+        users.map( user => (
+          <li key={ user.id } style={{ display: 'flex', justifyContent: 'space-around'}}>
+            <div style={{ flex: 1 }}>
+              { user.name }
+            </div>
+            <div style={{ flex: 1 }}>
+              { user.active ? 'active': 'inactive'}
+            </div>
+            <div style={{ flex: 1 }}>
+              { user.state }
+            </div>
+            <div style={{ flex: 1 }}>
+              { user.role }
+            </div>
+          </li>
+        ))
+
+      }
+      </ul>
     </div>
   );
 };
 
 const defaultFilter = {
-  active: true,
-  state: 'NY',
-  role: 'admin'
 };
 
 const Routes = ()=> {
   return (
     <Router>
       <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to={`/users?where=${JSON.stringify(defaultFilter)}`}>Users</Link></li>
+        <ul style={{ display: 'flex'}}>
+          <li style={{ flex: 1}}><Link to='/'>Home</Link></li>
+          <li style={{ flex: 1}}><Link to={`/users?where=${JSON.stringify(defaultFilter)}`}>Users</Link></li>
         </ul>
         <Route path='/' exact component= { Home } />
         <Route path='/users' exact component= { Users } />
